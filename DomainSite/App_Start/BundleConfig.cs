8643f29f0c3f486a807c82927939b5128d879630
Joseph Cenatiempo <joe@cenatech.com>
2019-08-13 18:51:15 -0400

    Work on integration to bootstrap 4 blog template

--- DomainSite/App_Start/BundleConfig.cs
+++ DomainSite/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace DomainSite
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css," +
+                      "~/Content/blog.css"));
         }
     }
 }
