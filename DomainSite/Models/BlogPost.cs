44c038f9e03fd35a9beea20c18cd6911184d0aba
Joseph Cenatiempo <joe@cenatech.com>
2019-08-13 20:15:46 -0400

    Refactor to use BlogPost pocos

--- DomainSite/Models/BlogPost.cs
+++ DomainSite/Models/BlogPost.cs
@@ -7,21 +7,18 @@ namespace DomainSite.Models
 {
     public class BlogPost
     {
-        private Category Category;
-        private TitleFull TitleFull;
-        private TitleShort TitleShort;
-        private DateTime DateCreated;
-        private DateTime DateLastModified;
-        private DateTime DatePublished;
-
-        private SummaryShort SummaryShort;
-        private SummaryLong SummaryLong;
-        private Body Body;
-
-        public BlogPost(Category Category)
-        {
-
-        }
+        public Category Category { get; set; }
+        public bool IsFeatured { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorEmail { get; set; }
+        public string TitleFull { get; set; }
+        public string TitleShort { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateLastModified { get; set; }
+        public DateTime DatePublished { get; set; }
+        public string SummaryShort { get; set; }
+        public string SummaryLong { get; set; }
+        public string Body { get; set; }
     }
 
     public enum Category
@@ -38,135 +35,5 @@ namespace DomainSite.Models
         Health,
         Style,
         Travel
-    }
-
-    internal class TitleShort
-    {
-        private readonly string Text;
-        
-        protected TitleShort(string text)
-        {
-            if (text == null)
-            {
-                throw new IndexOutOfRangeException("TitleShort expects a non-null string, input was null.");
-            } 
-            else if(text.Length < 1 || text.Length > 15)
-            {
-                throw new IndexOutOfRangeException("TitleShort expects a string between 1 and 15 characters. Input was '" + text + "' (" + text.Length + " chars).");
-            }
-            else
-            {
-                this.Text = text;
-            }
-        }
-        
-        public override string ToString()
-        {
-            return this.Text;
-        }
-    }
-
-    internal class TitleFull
-    {
-        private readonly string Text;
-
-        protected TitleFull(string text)
-        {
-            if (text == null)
-            {
-                throw new IndexOutOfRangeException("TitleFull expects a non-null string, input was null.");
-            }
-            else if (text.Length < 1 || text.Length > 50)
-            {
-                throw new IndexOutOfRangeException("TitleFull expects a string between 1 and 50 characters. Input was '" + text + "' (" + text.Length + " chars).");
-            }
-            else
-            {
-                this.Text = text;
-            }
-        }
-
-        public override string ToString()
-        {
-            return this.Text;
-        }
-    }
-
-    internal class SummaryShort
-    {
-        private readonly string Text;
-
-        protected SummaryShort(string text)
-        {
-            if (text == null)
-            {
-                throw new IndexOutOfRangeException("SummaryShort expects a non-null string, input was null.");
-            }
-            else if (text.Length < 1 || text.Length > 30)
-            {
-                throw new IndexOutOfRangeException("SummaryShort expects a string between 1 and 30 characters. Input was '" + text + "' (" + text.Length + " chars).");
-            }
-            else
-            {
-                this.Text = text;
-            }
-        }
-
-        public override string ToString()
-        {
-            return this.Text;
-        }
-    }
-
-    internal class SummaryLong
-    {
-        private readonly string Text;
-
-        protected SummaryLong(string text)
-        {
-            if (text == null)
-            {
-                throw new IndexOutOfRangeException("SummaryLong expects a non-null string, input was null.");
-            }
-            else if (text.Length < 1 || text.Length > 255)
-            {
-                throw new IndexOutOfRangeException("SummaryLong expects a string between 1 and 255 characters. Input was '" + text + "' (" + text.Length + " chars).");
-            }
-            else
-            {
-                this.Text = text;
-            }
-        }
-
-        public override string ToString()
-        {
-            return this.Text;
-        }
-    }
-
-    internal class Body
-    {
-        private readonly string Text;
-
-        protected Body(string text)
-        {
-            if (text == null)
-            {
-                throw new IndexOutOfRangeException("Body expects a non-null string, input was null.");
-            }
-            else if (text.Length < 1 || text.Length > 10000)
-            {
-                throw new IndexOutOfRangeException("Body expects a string between 1 and 10,000 characters. Input was '" + text + "' (" + text.Length + " chars).");
-            }
-            else
-            {
-                this.Text = text;
-            }
-        }
-
-        public override string ToString()
-        {
-            return this.Text;
-        }
     }
 }
