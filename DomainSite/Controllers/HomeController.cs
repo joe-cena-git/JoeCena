a91f487d3ae67c30499fb8a832fe557a48f11477
Joseph Cenatiempo <joe@cenatech.com>
2019-08-14 19:41:35 -0400

    Vastly simplify site to be mostly hard-coded

--- DomainSite/Controllers/HomeController.cs
+++ DomainSite/Controllers/HomeController.cs
@@ -4,35 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using DomainSite.Models;
-using DomainSite.Services;
 
 namespace DomainSite.Controllers
 {
     public class HomeController : Controller
     {
-        private PostService PostService = null;
-
-        private PostService GetSingletonPostService()
-        {
-            if(this.PostService == null)
-            {
-                this.PostService = new PostService();
-            }
-            return this.PostService;
-        }
-
-        [HttpGet]
         public ActionResult Index()
         {
-            var postService = GetSingletonPostService();
-            return View(postService.Get());
-        }
-
-        [HttpGet]
-        public ActionResult Get(int id)
-        {
-            var blogPostService = GetSingletonPostService();
-            return View(blogPostService.Get(id));
+            return View();
         }
     }
 }
