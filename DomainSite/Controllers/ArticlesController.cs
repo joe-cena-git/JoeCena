fdd6937e1c467cef603a3b196fd725809c27afa8
Joseph Cenatiempo <joe@cenatech.com>
2019-08-24 08:29:46 -0400

    Add article controller

---
+++ DomainSite/Controllers/ArticlesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DomainSite.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        // GET: Article
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
