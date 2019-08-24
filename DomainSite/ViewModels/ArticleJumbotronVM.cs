d5d164ff553d7715233aa7a2201a2df43a940671
Joseph Cenatiempo <joe@cenatech.com>
2019-08-24 08:25:21 -0400

    Add partial view for all article jumbotrons

---
+++ DomainSite/ViewModels/ArticleJumbotronVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DomainSite.ViewModels
+{
+    public class ArticleJumbotronVM
+    {
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public DateTime PublishDate { get; set; }
+        public DateTime RevisedDate { get; set; }
+    }
+}
